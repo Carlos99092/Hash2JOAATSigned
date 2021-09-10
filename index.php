d6c90954a0da4517b7ce9b51f99141787574a103
Carlos9909 <carlus9909@gmail.com>
2021-09-10 10:58:46 +0200

    Release

---
+++ index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en" >
+  <head>
+    <meta charset="UTF-8">
+    <title>Hash2JOAATUnsigned</title>
+    <link rel="stylesheet" href="./style.css">
+  </head>
+  <body>
+    <form method="post">  
+      <textarea rows='1' name="prop" type="text" placeholder="Insert data"></textarea>
+      <input  type="submit" name="submit" value="HASH">  
+    </form>  
+    <script  src="./script.js"></script>
+  </body>
+</html>
+
+
+<?php
+if (isset($_POST['submit']))
+{
+
+    error_reporting(E_ERROR | E_PARSE);
+    $text = trim($_POST['prop']);
+    $textAr = explode("\n", $text);
+    $textAr = array_filter($textAr, 'trim');
+    foreach ($textAr as $texto)
+    {
+        $texto = preg_replace('/\s+/', '', $texto);
+        $hash = hash('joaat', $texto, false);
+        $tmp = reset(unpack("l", pack("l", hexdec($hash))));
+        echo reset(unpack("l", pack("l", hexdec($hash))));
+        echo "<br>";
+
+    }
+
+}
+?>
