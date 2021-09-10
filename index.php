46898307ba2b26731bd0c338073b28530c4ac16b
Carlos9909 <carlus9909@gmail.com>
2021-09-10 11:02:20 +0200

    Update index.php

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <html lang="en" >
   <head>
     <meta charset="UTF-8">
-    <title>Hash2JOAATUnsigned</title>
+    <title>Hash2JOAATSigned</title>
     <link rel="stylesheet" href="./style.css">
   </head>
   <body>
